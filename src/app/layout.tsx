64c26c0ba663424fa04b970ad62f852ad6743b17
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-03 16:43:26 -0300

    Style: Added images and icons to main screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Spideverse",
@@ -14,7 +15,21 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
-        <header></header>
+        <header>
+          <Image
+            src="/icons/menu.svg"
+            alt="Opções de Menu"
+            width={36}
+            height={25}
+          />
+          <Image
+            src="spider-logo.svg"
+            alt="spider-man"
+            width={260}
+            height={70}
+          />
+          <Image src="/icons/user.svg" alt="Login" width={36} height={25} />
+        </header>
         {children}
       </body>
     </html>
