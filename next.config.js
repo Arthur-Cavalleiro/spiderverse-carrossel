2bc5b0fa3258fa7ec92bda2c3b73e521d94b949c
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-03 16:26:44 -0300

    added api route from mockapi

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    API_URL: "https://6595abed04335332df832196.mockapi.io",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
