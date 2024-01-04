53b40ffb6160a9ff80abefbb3481115034b004ca
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-04 08:53:10 -0300

    feat(mainPage): consuming api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>ye123</h1>;
+import HeroesList from "@/components";
+import { IHeroData } from "@/interfaces/heroes";
+import styles from "./page.module.scss";
+
+async function getHeroesData(): Promise<{ data: IHeroData[] }> {
+  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+
+  if (!res.ok) {
+    throw new Error("Failed to request heroes list");
+  }
+
+  return res.json();
+}
+export default async function Home() {
+  const heroes = await getHeroesData();
+
+  return (
+    <main className={styles.main}>
+      <HeroesList heroes={heroes.data} />
+    </main>
+  );
 }
---
+++ src/components/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { IHeroData } from "@/interfaces/heroes";
+
+interface IProps {
+  heroes: IHeroData[];
+}
+
+export default function HeroesList({ heroes }: IProps) {
+  return (
+    <>
+      <h1>personagens</h1>
+      {heroes.map((hero) => (
+        <p key={hero.id}>{hero.name}</p>
+      ))}
+    </>
+  );
+}
