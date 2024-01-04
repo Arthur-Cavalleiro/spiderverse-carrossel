d4120d9aa60ff723b76fa9a4a81573e621ca1bd8
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-04 12:02:00 -0300

    feat: component for Images containers

---
+++ src/components/HeroPicture/index.tsx
@@ -0,0 +1,33 @@
+import { IHeroData } from "@/interfaces/heroes";
+import Image, { StaticImageData } from "next/image";
+import ImageSpiderMan616 from "@public/spiders/spider-man-616.png";
+import ImageSpiderWoman65 from "@public/spiders/mulher-aranha-65.png";
+import ImageSpiderMan1610 from "@public/spiders/spider-man-1610.png";
+import ImagemSpiderMan14512 from "@public/spiders/sp-dr-14512.png";
+import ImagemSpiderHam8311 from "@public/spiders/spider-ham-8311.png";
+import ImagemSpiderMan90214 from "@public/spiders/spider-man-90214.png";
+import ImagemSpiderMan928 from "@public/spiders/spider-man-928.png";
+
+const heroesImage: Record<string, StaticImageData> = {
+  "spider-man-616": ImageSpiderMan616,
+  "mulher-aranha-65": ImageSpiderWoman65,
+  "spider-man-1610": ImageSpiderMan1610,
+  "sp-dr-14512": ImagemSpiderMan14512,
+  "spider-ham-8311": ImagemSpiderHam8311,
+  "spider-man-90214": ImagemSpiderMan90214,
+  "spider-man-928": ImagemSpiderMan928,
+};
+
+interface IProps {
+  hero: IHeroData;
+}
+
+export default function HeroPicture({ hero }: IProps) {
+  return (
+    <Image
+      src={heroesImage[hero.id]}
+      alt={`${hero.name} (Universo-${hero.universe})`}
+      priority
+    />
+  );
+}
--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -2,6 +2,7 @@
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 import styles from "./heroesList.module.scss";
+import HeroPicture from "../HeroPicture";
 
 interface IProps {
   heroes: IHeroData[];
@@ -13,9 +14,16 @@ export default function HeroesList({ heroes }: IProps) {
       <h1 className={`${spidermanFont.className} ${styles.title}`}>
         personagens
       </h1>
-      {heroes.map((hero) => (
-        <p key={hero.id}>{hero.name}</p>
-      ))}
+      <section className={styles.heroes}>
+        {heroes.map((hero) => (
+          <div
+            key={hero.id}
+            className={`${styles.imageContainer} ${styles[hero.id]}`}
+          >
+            <HeroPicture hero={hero} />
+          </div>
+        ))}
+      </section>
     </>
   );
 }
