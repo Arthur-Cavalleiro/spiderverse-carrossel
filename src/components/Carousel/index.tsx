f3bc2e3b304eab5266f6484208fb722d61c90d1c
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-05 11:59:03 -0300

    Feat: Carousel on progress

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -4,7 +4,13 @@ import HeroDetails from "../HeroDetails";
 import styles from "./carousel.module.scss";
 import { useEffect, useState } from "react";
 import HeroPicture from "../HeroPicture";
+import { AnimatePresence, motion } from "framer-motion";
 
+enum enPosition {
+  FRONT = 0,
+  MIDDLE = 1,
+  BACK = 2,
+}
 interface IProps {
   heroes: IHeroData[];
   activeId: string;
@@ -27,6 +33,10 @@ export default function Carousel({ heroes, activeId }: IProps) {
     setvisibleItems(visibleIems);
   }, [heroes, activeIndex]);
 
+  const handleChangeActiveIndex = (newDirection: number) => {
+    setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
+  };
+
   if (!visibleItems) {
     return null;
   }
@@ -34,12 +44,24 @@ export default function Carousel({ heroes, activeId }: IProps) {
   return (
     <div className={styles.container}>
       <div className={styles.carousel}>
-        <div className={styles.wrapper}>
-          {visibleItems.map((item) => (
-            <div key={item.id} className={styles.hero}>
-              <HeroPicture hero={item} />
-            </div>
-          ))}
+        <div
+          className={styles.wrapper}
+          onClick={() => handleChangeActiveIndex(1)}
+        >
+          <AnimatePresence mode="popLayout">
+            {visibleItems.map((item, position) => (
+              <motion.div
+                key={item.id}
+                className={styles.hero}
+                initial={{ x: -1500, scale: 0.75 }}
+                animate={{ x: 0, ...getItemsStyles(position) }}
+                exit={{ x: 0, opacity: 0, scale: 1, left: "-20%" }}
+                transition={{ duration: 0.8 }}
+              >
+                <HeroPicture hero={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
         </div>
       </div>
 
@@ -49,3 +71,30 @@ export default function Carousel({ heroes, activeId }: IProps) {
     </div>
   );
 }
+
+const getItemsStyles = (position: enPosition) => {
+  if (position === enPosition.FRONT) {
+    return {
+      zIndex: 3,
+      filter: "blur(10px)",
+      scale: 1.2,
+    };
+  }
+  if (position === enPosition.MIDDLE) {
+    return {
+      zIndex: 2,
+      left: 300,
+      scale: 0.8,
+      top: "-10%",
+    };
+  } else {
+    return {
+      zIndex: 1,
+      filter: "blur(10px)",
+      left: 160,
+      top: "-20%",
+      scale: 0.6,
+      opacity: 0.8,
+    };
+  }
+};
