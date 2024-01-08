cf48c706fb4ed558dd99836859057c83871939c1
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-08 14:26:04 -0300

    build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   env: {
     API_URL: "https://6595abed04335332df832196.mockapi.io",
     DOMAIN_ORIGIN:
-      "https://spiderverse-carrossel-myversion-git-main-arthur-cavalleiro.vercel.app",
+      "https://spiderverse-carrossel-git-main-arthur-cavalleiro.vercel.app",
   },
 };
 
