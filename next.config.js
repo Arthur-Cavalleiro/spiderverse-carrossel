49db618768e4ff336cb31d78a36aafad2d1512b7
Arthur-Cavalleiro <arthurcmb84@gmail.com>
2024-01-08 14:49:43 -0300

    Fix: unecessary "/"

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://6595abed04335332df832196.mockapi.io",
-    DOMAIN_ORIGIN: "https://spiderverse-carrossel-myversion.vercel.app/",
+    DOMAIN_ORIGIN: "https://spiderverse-carrossel-myversion.vercel.app",
   },
 };
 
